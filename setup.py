461338023ce59ad6eeb4be85b902174ba2743456
someshs <someshs@adobe.com>
2023-10-19 19:19:33 +0530

    Use smptlib

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup
 
 setup(
-    name="notification",
-    version="0.1",
-    packages=["notification"],
+    name='notification',
+    version='0.1',
+    packages=['notification'],
     install_requires=[
-        "yagmail==0.14.260",
+        'certifi==2021.5.30',
+        'charset-normalizer==2.0.10',
+        'idna==3.3',
+        'requests==2.26.0',
+        'smtplib==0.0.1',
+        'urllib3==1.26.6',
     ],
     entry_points={
-        "console_scripts": [
-            "send_email=notification.send_email:main",
+        'console_scripts': [
+            'send_email_gmail=notification.send_email_gmail:main',
         ],
     },
 )
