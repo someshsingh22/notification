878944e6e232b516b5e61ec1cd439c1991d898b6
someshs <someshs@adobe.com>
2023-10-19 19:18:49 +0530

    Use smptlib

--- notification/send_email.py
+++ notification/send_email.py
@@ -1,40 +1,59 @@
 import os
-import yagmail
-import logging
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 def send_email(subject, body, recipient_email):
+    # Load email and password from environment variables
     sender_email = os.environ.get("EMAIL_USERNAME")
     sender_password = os.environ.get("EMAIL_PASSWORD")
-    yag = yagmail.SMTP(sender_email, sender_password)
-    print("SMTP connection established.")
-    yag.send(to=recipient_email, subject=subject, contents=body)
-    yag.close()
-    print("Email sent successfully!")
 
+    # Check if email credentials are provided
+    if sender_email is None or sender_password is None:
+        print("Error: Email credentials not provided.")
+        exit(1)
 
-def main():
-    import argparse
+    # Set a default recipient email address if not provided
+    if recipient_email is None:
+        recipient_email = "singhksomesh@gmail.com"
 
-    parser = argparse.ArgumentParser(description="Send an email using yagmail.")
-    parser.add_argument("subject", type=str, help="Subject of the email")
-    parser.add_argument("body", type=str, help="Body of the email")
-    parser.add_argument("recipient_email", type=str, help="Recipient email address", default="singhksomesh@gmail.com")
-    args = parser.parse_args()
-    
-    assert os.environ.get("EMAIL_USERNAME") is not None, "EMAIL_USERNAME not set"
-    assert os.environ.get("EMAIL_PASSWORD") is not None, "EMAIL_PASSWORD not set"
-    assert args.subject is not None, "Subject not provided"
-    assert args.body is not None, "Body not provided"
-    
-    if args.recipient_email is None:
-        print(
-            "No recipient email provided. Using sender email as recipient email."
-        )
-        send_email(args.subject, args.body, os.environ.get("EMAIL_USERNAME"))
-    else:
-        send_email(args.subject, args.body, args.recipient_email)
+    # Set up the MIME
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = recipient_email
+    message["Subject"] = subject
+
+    # Attach the body of the email
+    message.attach(MIMEText(body, "plain"))
+
+    # Connect to the Gmail SMTP server
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        # Login to your email account
+        server.login(sender_email, sender_password)
+
+        # Convert the message to a string
+        text = message.as_string()
+
+        # Send the email
+        server.sendmail(sender_email, recipient_email, text)
+
+    print("Email sent successfully.")
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+
+    # Initialize argparse
+    parser = argparse.ArgumentParser(description="Send an email using Gmail SMTP.")
+
+    # Add command-line arguments
+    parser.add_argument("subject", type=str, help="Subject of the email")
+    parser.add_argument("body", type=str, help="Body of the email")
+    parser.add_argument("--recipient_email", type=str, help="Recipient email address")
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Extract arguments and call the send_email function
+    send_email(args.subject, args.body, args.recipient_email)
