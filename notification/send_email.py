a0dd2d39f65e0b24d05c77a748baab80a177b948
someshs <someshs@adobe.com>
2023-10-19 19:07:46 +0530

    Notification Sender

--- notification/send_email.py
+++ notification/send_email.py
@@ -1,49 +1,33 @@
 import os
 import yagmail
+import logging
 
 
 def send_email(subject, body, recipient_email):
-    # Load email and password from environment variables
     sender_email = os.environ.get("EMAIL_USERNAME")
     sender_password = os.environ.get("EMAIL_PASSWORD")
-
-    # Check if email credentials are provided
-    if sender_email is None or sender_password is None:
-        print("Error: Email credentials not provided.")
-        exit(1)
-
-    # Set a default recipient email address if not provided
-    if recipient_email is None:
-        recipient_email = "someshs@gmail.com"
-
-    # Create the yagmail SMTP client
     yag = yagmail.SMTP(sender_email, sender_password)
-
-    # Send the email
+    logging.info("SMTP connection established.")
     yag.send(to=recipient_email, subject=subject, contents=body)
-
-    # Close the SMTP connection
     yag.close()
-
-    print("Email sent successfully.")
+    logging.info("Email sent successfully!")
 
 
 def main():
     import argparse
 
-    # Initialize argparse
     parser = argparse.ArgumentParser(description="Send an email using yagmail.")
-
-    # Add command-line arguments
-    parser.add_argument("subject", type=str, help="Subject of the email")
-    parser.add_argument("body", type=str, help="Body of the email")
-    parser.add_argument("--recipient_email", type=str, help="Recipient email address")
-
-    # Parse the command-line arguments
+    parser.add_argument("subject", type=str, help="Subject of the email", required=True)
+    parser.add_argument("body", type=str, help="Body of the email", required=True)
+    parser.add_argument("recipient_email", type=str, help="Recipient email address")
     args = parser.parse_args()
-
-    # Extract arguments and call the send_email function
-    send_email(args.subject, args.body, args.recipient_email)
+    if args.recipient_email is None:
+        logging.info(
+            "No recipient email provided. Using sender email as recipient email."
+        )
+        send_email(args.subject, args.body, os.environ.get("EMAIL_USERNAME"))
+    else:
+        send_email(args.subject, args.body, args.recipient_email)
 
 
 if __name__ == "__main__":
